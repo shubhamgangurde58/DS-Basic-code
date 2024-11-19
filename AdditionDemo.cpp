e32e5e6b2fa7f41bc610d557a93d0e0ef1370781
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-11-19 21:04:21 +0530

    [ADDED]: shubham this code for addition Demo1

---
+++ AdditionDemo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	int c;
+	cout<<"please Enter the two no:"<<endl;
+	cin>>a>>b;
+	c=a+b;
+	cout<<"Addition="<<c;
+	return 0;
+}
